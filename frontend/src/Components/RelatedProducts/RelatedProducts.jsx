63e7119b00735f4b419bd3ac1c77ac444bf90898
Vishnu-9014 <159953291+Vishnu-9014@users.noreply.github.com>
2025-07-11 00:59:14 +0530

    Update RelatedProducts.jsx

--- frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -7,7 +7,7 @@ const RelatedProducts = () => {
 
   const [all_product,setAll_Product]=useState([]);
   useEffect(()=>{
-        fetch('http://localhost:4000/allproducts')
+        fetch('https://e-commerce-backend-r3ez.onrender.com/allproducts')
         .then((response)=>response.json())
         .then((data)=>setAll_Product(data));
       })
@@ -20,7 +20,6 @@ const RelatedProducts = () => {
         {all_product.map((item,i)=>{
             return <Item  key={i} id={item.id} name={item.name} image={item.image}
          new_price={item.new_price} old_price={item.old_price}/>
-            
         })}
       </div>
     </div>
