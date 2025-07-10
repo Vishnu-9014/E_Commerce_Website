dc805e0750ad04c8cc8787802cdc25b87b439379
Vishnu-9014 <159953291+Vishnu-9014@users.noreply.github.com>
2025-07-11 01:01:26 +0530

    Update LoginSignup.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -3,32 +3,32 @@ import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
 
-  const [state,setState]=useState("Login");
+  const [state, setState] = useState("Login");
 
-  const [formData,setFormData]=useState({
-    username:"",
-    password:"",
-    email:"",
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: "",
   })
 
-  const changeHandler=(e)=>{
-    setFormData({...formData,[e.target.name]:e.target.value})
+  const changeHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
   }
 
-  const login = async ()=>{
-    console.log("Login function executed",formData);
+  const login = async () => {
+    console.log("Login function executed", formData);
     let responseData;
-    await fetch('http://localhost:4000/login',{
-      method:'POST',
-      headers:{
-        Accept:'application/json',
-        'Content-Type':'application/json',
+    await fetch('https://e-commerce-backend-r3ez.onrender.com/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       },
-      body:JSON.stringify(formData),
-    }).then((response)=>response.json())
-    .then((data)=>responseData=data);
-    if(responseData.success){
-      localStorage.setItem('auth-token',responseData.token);
+      body: JSON.stringify(formData),
+    }).then((response) => response.json())
+      .then((data) => responseData = data);
+    if (responseData.success) {
+      localStorage.setItem('auth-token', responseData.token);
       window.location.replace("/");
     }
     else {
@@ -36,50 +36,46 @@ const LoginSignup = () => {
     }
   }
 
-  const signup=async ()=>{
-    console.log("signup is executed",formData);
+  const signup = async () => {
+    console.log("signup is executed", formData);
     let responseData;
-    await fetch('http://localhost:4000/signup',{
-      method:'POST',
-      headers:{
-        Accept:'application/json',
-        'Content-Type':'application/json',
+    await fetch('https://e-commerce-backend-r3ez.onrender.com/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       },
-      body:JSON.stringify(formData),
-    }).then((response)=>response.json())
-    .then((data)=>responseData=data);
-    if(responseData.success){
-      localStorage.setItem('auth-token',responseData.token);
+      body: JSON.stringify(formData),
+    }).then((response) => response.json())
+      .then((data) => responseData = data);
+    if (responseData.success) {
+      localStorage.setItem('auth-token', responseData.token);
       window.location.replace("/");
     }
     else {
       alert(responseData.errors)
     }
   }
-
-
 
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
         <h1>{state}</h1>
         <div className="loginsignup-fields">
-          {state==="Sign Up"?<input  name='username' value={formData.username} onChange={changeHandler}  type="text" placeholder='Your Name' />:<></>}
+          {state === "Sign Up" ? <input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder='Your Name' /> : <></>}
           <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder='Email' />
           <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder='Password' />
         </div>
-        <button onClick={()=>{
-          {state==="Login"?login():signup()}
+        <button onClick={() => {
+          { state === "Login" ? login() : signup() }
         }}>Continue</button>
 
-        {state==="Sign Up"?<p className="loginsignup-login">
-          Already have an account? <span onClick={()=>{setState("Login")}}>Login here</span>
-        </p>:<p className="loginsignup-login">
-          Create an account? <span onClick={()=>{setState("Sign Up")}}>Click here</span>
+        {state === "Sign Up" ? <p className="loginsignup-login">
+          Already have an account? <span onClick={() => { setState("Login") }}>Login here</span>
+        </p> : <p className="loginsignup-login">
+          Create an account? <span onClick={() => { setState("Sign Up") }}>Click here</span>
         </p>}
 
-        
-        
         <div className="loginsignup-agree">
           <input type="checkbox" />
           <p>By continuing, I agree to the terms of use & privacy policy.</p>
